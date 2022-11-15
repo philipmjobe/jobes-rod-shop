f7ab5e810da3bff585b0badf73684bc9c1a8ef18
Philip <philipmjobe@gmail.com>
2022-11-15 12:51:08 -0500

    adds some more styling to get the background to look right

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import jobesrodshop from "./assets/jobesrodshop.PNG";
 import "./App.css";
-import background from "./assets/pexels-mi.jpg";
 import DarkVariantExample from "./components/Carousel.js";
 import ContactForm from "./components/ContactForm.js";
 
 function App() {
   return (
-    <div
-      className="App"
-      style={{ backgroundImage: `url(${background})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}
-    >
+    <div className="App">
       <header className="App-header">
         <img src={jobesrodshop} className="App-logo" alt="logo" />
       </header>
