2f542a8d3e0f818c8a66f7a304bd1b5ff474b4ff
Philip <philipmjobe@gmail.com>
2022-11-18 11:35:58 -0500

    adds facebook button and styles to get to top left corner

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import jobesrodshop from "./assets/jobesrodshop.PNG";
 import "./App.css";
 import DarkVariantExample from "./components/Carousel.js";
 import ContactForm from "./components/ContactForm.js";
+import { MDBBtn, MDBIcon } from "mdb-react-ui-kit";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="facebook-button">
+          <MDBBtn
+            className="m-1"
+            style={{ backgroundColor: "#3b5998" }}
+            href="https://www.facebook.com/Jobesbustedrodshop"
+          >
+            <p>Find Us On Facebook</p>
+            <MDBIcon fab icon="facebook-f" />
+          </MDBBtn>
+        </div>
         <img src={jobesrodshop} className="App-logo" alt="logo" />
       </header>
       <DarkVariantExample />
