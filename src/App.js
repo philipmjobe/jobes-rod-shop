5deacc2a9ec740746d8254a4ba1b00e9c4c1532b
Philip <philipmjobe@gmail.com>
2022-11-27 17:51:21 -0500

    adds some more css to try to scale

--- src/App.js
+++ src/App.js
@@ -5,37 +5,18 @@ import ContactForm from "./components/ContactForm.js";
 import { MDBBtn, MDBIcon } from "mdb-react-ui-kit";
 import { MDBFooter } from "mdb-react-ui-kit";
 import background from "./assets/pexels-mi.jpg";
-import { useMediaQuery } from "react-responsive";
 
 const App = () => {
-  const isDesktop = useMediaQuery({
-    query: "(min-width: 1224px)",
-  });
-
-  const isTablet = useMediaQuery({
-    query: "(max-width: 1224px)",
-  });
-
-  const isMobile = useMediaQuery({
-    query: "(max-width: 786px)",
-  });
-
-  const isPortrait = useMediaQuery({
-    query: "(orientation: portrait)",
-  });
-
-  const isRetina = useMediaQuery({
-    query: "(max-resolution: 300dpi)",
-  });
   return (
     <div
       className="App"
-      style={{ backgroundImage: `url(${background})`, backgroundAttachment: "fixed", backgroundSize: "cover" }}
+      style={{
+        backgroundImage: `url(${background})`,
+        // backgroundAttachment: "fixed",
+        backgroundSize: "cover",
+        // width: "100vh",
+      }}
     >
-      {isDesktop}
-      {isTablet}
-      {isPortrait ? "portrait" : "landscape"}
-      {isRetina}
       <header className="App-header">
         <div className="phone-number">
           <p>Give Me a Call at (740)507-2522</p>
